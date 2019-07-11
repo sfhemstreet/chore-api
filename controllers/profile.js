aabc819b4b108587ddc0735ec32afe50d8fc04a7
sfhemstreet <sfhemstreet@gmail.com>
2019-07-10 18:35:31 -0700

    profile route

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,13 +1,18 @@
-const handleGetAllGroups = (req,res,db) => {
+const parseData = require('../util/parseData');
 
+const handleGetAllGroups = (req,res,db) => {
+    console.log('req.session at profile',req.session.user_id)
+    
     // see if user has session
     if(req.session.user_id){
+        // get all chores from groups associated with this user  
         return db.select('choregroup.group_id')
             .from('choregroup')
             .join('chore', 'choregroup.group_id','=','chore.group_id')
             .join('users','users.user_id','=','chore.assign_id')
             .where('users.user_id', '=', req.session.user_id)
-            .returning(groupID => {
+            .returning('choregroup.group_id')
+            .then(groupID => {
                 db.select('users.user_name AS assign_name', 
                     'users.email AS assign_email',
                     'chore.chore_name',
@@ -17,10 +22,17 @@ const handleGetAllGroups = (req,res,db) => {
                     'choregroup.group_name').from('chore')
                     .join('choregroup','choregroup.group_id','=','chore.group_id')
                     .join('users','chore.assign_id','=','users.user_id')
-                    .where('choregroup.group_id','=', groupID)
+                    .where(() => {
+                        for(let x = 0; x < groupID.length; x++){
+                            ('choregroup.group_id','=', groupID[x].group_id)
+                        }
+                    }) // THIS NEEDS TO GET ALL GROUPS!
                     .then(data => {
+                        console.log(data)
+                        // organize chore data into groups
+                        //const organizedGroups = parseData.organizeGroupData(data)
                         res.json({
-                            groups: data
+                            groups: data//organizedGroups
                         })  
                     })
                     .catch(error => {
@@ -32,6 +44,7 @@ const handleGetAllGroups = (req,res,db) => {
         
     }
     else{
+        console.log(req.session.user_id)
         res.status(403).json('BOI')
     }
     
--- controllers/signin.js
+++ controllers/signin.js
@@ -2,7 +2,7 @@
 const key = require('../key');
 
 // HANDLE USER SIGN IN
-const handleSignin = (req,res,db, bcrypt, session) => {
+const handleSignin = (req,res,db,bcrypt) => {
     const {email, password} = req.body;
     // DO MORE CHECKS HERE
     if(!email || !password){
@@ -20,11 +20,39 @@ const handleSignin = (req,res,db, bcrypt, session) => {
                         .where('users.email','=',email)
                         .then(userInfo => {
                             const {email, score, user_name, user_id} = userInfo[0];
+                            // data we send to user
                             const user = {user_name, email, score};
+                            // set session user_id
                             req.session.user_id = user_id;
-                            res.json({
-                                userData: user
-                            });  
+                            // get all group/chore info that user is apart of
+                            db.select('choregroup.group_id')
+                            .from('choregroup')
+                            .join('chore', 'choregroup.group_id','=','chore.group_id')
+                            .join('users','users.user_id','=','chore.assign_id')
+                            .where('users.user_id', '=', req.session.user_id)
+                            .returning('choregroup.group_id')
+                            .then(groupID => {
+                                db.select('users.user_name AS assign_name', 
+                                    'users.email AS assign_email',
+                                    'chore.chore_name',
+                                    'chore.assign_date', 
+                                    'chore.due_date', 
+                                    'chore.complete_date', 
+                                    'choregroup.group_name').from('chore')
+                                    .join('choregroup','choregroup.group_id','=','chore.group_id')
+                                    .join('users','chore.assign_id','=','users.user_id')
+                                    .where('choregroup.group_id','=', groupID[0].group_id)
+                                    .then(groupData => {
+                                        res.json({
+                                            groups: groupData,
+                                            userData: user
+                                        })  
+                                    })
+                                    .catch(error => {
+                                        //console.log(error)
+                                        res.status(400).json('group db problem')
+                                    });
+                            })
                         })
                         .catch(error => res.status(400).json('Unable to get user'))
                 }
