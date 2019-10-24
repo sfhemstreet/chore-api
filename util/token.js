8c0f6a18c152b8c8ded9684dbb2e18702b7e6d5f
sfhemstreet <sfhemstreet@gmail.com>
2019-10-23 21:36:46 -0700

    token expire change

--- util/token.js
+++ util/token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const createToken = (data) => {
     return new Promise((resolve, reject) => {
-        jwt.sign({ data: data }, process.env.TOKEN_SECRET, { algorithm: 'HS256', expiresIn: '1h' }, (err, token) => {
+        jwt.sign({ data: data }, process.env.TOKEN_SECRET, { algorithm: 'HS256', expiresIn: '2d' }, (err, token) => {
             if(err){
                 reject(null);
             }
